2fa9ab7b1849d277f20a46b62290f65ccf1d6818
aman8920 <8744phenol@gmail.com>
2024-09-09 13:04:45 +0530

    first commit

---
+++ src/com/aman/Basic.java
@@ -0,0 +1,15 @@
+package src.com.aman;
+
+class Basic{
+    public static void main(String[] args){
+        //int x=10;
+        int x=9;
+        String s1 = null;
+        String s2 = "aman";
+        String s3 = new String("hello");
+        System.out.println(x);
+        System.out.println(s1);
+        System.out.println(s2==s3);
+    }
+
+}
---
+++ src/com/aman/Operators.java
@@ -0,0 +1,12 @@
+package src.com.aman;
+
+// package com.aman.basic ;
+
+
+public class Operators {
+    public static void main(String[] args) {
+        int s = 10;
+        System.out.println(s);
+    }
+    
+}
